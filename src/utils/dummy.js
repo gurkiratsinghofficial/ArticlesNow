4803b99ce13454927509ada3b3126129277165ab
gurkiratsinghofficial <gurkiratsinghofficial@gmail.com>
2022-03-22 21:47:39 +0530

    empty

--- src/utils/dummy.js
+++ src/utils/dummy.js
@@ -32,7 +32,7 @@ const articles = [
     id: 6,
     title: "consectetur adipiscing elit.",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed urna egestas, malesuada ligula ac, accumsan massa. Morbi aliquam feugiat bibendum. Ut vitae gravida lorem, sit amet ornare nibh. Cras aliquet dolor sed est consequat, in blandit ipsum laoreet. Sed mattis dui cursus nibh malesuada efficitur. Nullam rhoncus, ante ac rhoncus lacinia, risus nunc rhoncus lorem, eu luctus leo velit a ligula. Sed non magna at nisi mattis viverra dapibus ut nibh. Donec accumsan ligula sit amet pulvinar euismod. Aliquam ut quam purus. Suspendisse interdum lacinia urna at tincidunt. In tempor velit lorem, non rutrum risus tristique et.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed urna egestas, malesuada ligula ac, accumsan massa. Morbi aliquam feugiat bibendum. Ut vitae gravida lorem, sit amet ornare nibh. Cras aliquet dolor sed est consequat, in blandit ipsum laoreet. Sed mattis dui cursus nibh malesuada efficitur. Nullam rhoncus, ante ac rhoncus lacinia, risus nunc rhoncus lorem, eu luctus leo velit a ligula. Sed non magna at nisi mattis viverra dapibus ut nibh. Donec accumsan ligula sit amet pulvinar euismod. Aliquam ut quam purus. Suspendisse interdum lacinia urna at tincidunt. In tempor velit lorem, non rutrum risus tristique",
   },
 ];
 export default articles;
